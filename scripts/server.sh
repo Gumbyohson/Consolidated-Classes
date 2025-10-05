e08d824cf557a3d59c13352a75d718f79722d735
Gumbyohson <Gumbyohson@gmail.com>
2025-10-05 22:46:47 +1100

    Making the mod file playable. Relocated some template and note data into a top level folder to prevent mod validity issues

--- scripts/server.sh
+++
@@ -1,5 +0,0 @@
-# Unused by VS Code tasks
-chmod +x "$(pwd)/scripts/build.sh"
-"$(pwd)/scripts/build.sh"
-echo "Launching game client..."
-dotnet "$VINTAGE_STORY/VintagestoryServer.dll" --tracelog --addModPath "$(pwd)/Releases"
