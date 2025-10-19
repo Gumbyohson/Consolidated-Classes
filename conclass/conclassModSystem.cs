ce628c9d9b80e5b3d10b1060aa4ba1275d8c171f
Gumbyohson <Gumbyohson@gmail.com>
2025-10-19 23:58:14 +1100

    Updated healing effectiveness. Corrected substrate requirements

--- conclass/conclassModSystem.cs
+++ conclass/conclassModSystem.cs
@@ -12,17 +12,17 @@ namespace conclass
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            Mod.Logger.Notification("Consolidated classes mod loaded: " + api.Side);
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("conclass:hello"));
+            Mod.Logger.Notification("Consolidated classes mod loaded server side: " + Lang.Get("conclass:hello"));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("conclass:hello"));
+            Mod.Logger.Notification("Consolidated classes mod loaded client side: " + Lang.Get("conclass:hello"));
         }
 
     }
